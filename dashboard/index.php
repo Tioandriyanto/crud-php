63555124922b8496ca83fdc81ae7717df2aa353c
Tio Andriyanto <tioandrianto72@gmail.com>
2025-04-27 19:24:59 +0700

    update icon

--- dashboard/index.php
+++ dashboard/index.php
@@ -22,7 +22,7 @@ $total_matakuliah = mysqli_fetch_array($matakuliah)[0];
       <div class="col-lg-3 col-md-6 col-sm-6 col-12">
         <div class="card card-statistic-1">
           <div class="card-icon bg-primary">
-            <i class="far fa-user"></i>
+            <i class="bi bi-person"></i>
           </div>
           <div class="card-wrap">
             <div class="card-header">
@@ -38,7 +38,7 @@ $total_matakuliah = mysqli_fetch_array($matakuliah)[0];
       <div class="col-lg-3 col-md-6 col-sm-6 col-12">
         <div class="card card-statistic-1">
           <div class="card-icon bg-danger">
-            <i class="far fa-user"></i>
+            <i class="bi bi-person"></i>
           </div>
           <div class="card-wrap">
             <div class="card-header">
@@ -54,7 +54,7 @@ $total_matakuliah = mysqli_fetch_array($matakuliah)[0];
       <div class="col-lg-3 col-md-6 col-sm-6 col-12">
         <div class="card card-statistic-1">
           <div class="card-icon bg-danger">
-            <i class="far fa-user"></i>
+            <i class="bi bi-person"></i>
           </div>
           <div class="card-wrap">
             <div class="card-header">
